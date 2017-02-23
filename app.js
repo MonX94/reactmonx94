7c70d96e4c5cce4fef3d06851da0ac4f1a536f77
MonX94 <romanokurg@gmail.com>
2017-02-23 22:15:09 +0200

    Финальный апдейт

--- app.js
+++ app.js
@@ -1,50 +1,78 @@
 "use strict";
 
-var Hero = React.createClass({
-  displayName: "Hero",
-
-  getInitialState: function getInitialState() {
-    return {
-      count: 0
-    };
-  },
-  HandleClick: function HandleClick() {
-    this.setState({ count: this.state.count + 1 });
-  },
-  render: function render() {
+var ImageCounter = function ImageCounter(props) {
     return React.createElement(
-      "div",
-      { className: "container" },
-      React.createElement(
         "div",
-        { className: "count" },
-        this.state.count
-      ),
-      React.createElement("img", { src: this.props.imageUrl, onClick: this.HandleClick }),
-      React.createElement(
-        "h1",
-        null,
-        this.props.title
-      ),
-      React.createElement(
-        "p",
-        { className: "subtitle" },
-        this.props.subtitle
-      )
+        { className: "image-counter" },
+        React.createElement(
+            "div",
+            { className: "count" },
+            props.count
+        ),
+        React.createElement("img", { src: props.imageUrl, onClick: props.onCount })
     );
-  }
+};
+var Hero = React.createClass({
+    displayName: "Hero",
+
+    getInitialState: function getInitialState() {
+        return { count: 0 };
+    },
+    HandleClick: function HandleClick() {
+        this.setState({
+            count: this.state.count + 1
+        });
+    },
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "container" },
+            React.createElement(ImageCounter, { imageUrl: this.props.imageUrl, count: this.state.count, onCount: this.HandleClick }),
+            React.createElement(
+                "h1",
+                null,
+                this.props.title
+            ),
+            React.createElement(
+                "p",
+                { className: "subtitle" },
+                this.props.subtitle
+            )
+        );
+    }
 });
-ReactDOM.render(React.createElement(
-  "div",
-  null,
-  React.createElement(Hero, {
+var App = React.createClass({
+    displayName: "App",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            null,
+            this.props.heroes.map(function (hero) {
+                return React.createElement(Hero, { key: hero.id, title: hero.title, subtitle: hero.subtitle, imageUrl: hero.imageUrl });
+            })
+        );
+    }
+});
+var data = [{
+    id: 1,
     title: "React",
-    subtitle: "\u0411\u0438\u0431\u043B\u0438\u043E\u0442\u0435\u043A\u0430 \u0434\u043B\u044F \u0441\u043E\u0437\u0434\u0430\u043D\u0438\u044F \u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044C\u0441\u043A\u0438\u0445 \u0438\u043D\u0442\u0435\u0440\u0444\u0435\u0439\u0441\u043E\u0432",
+    subtitle: "Библиотека для создания пользовательских интерфейсов",
     imageUrl: "https://facebook.github.io/react/img/logo.svg"
-  }),
-  React.createElement(Hero, {
+}, {
+    id: 2,
     title: "Angular",
-    subtitle: "\u041E\u0434\u0438\u043D \u0444\u0440\u0435\u0439\u043C\u0432\u043E\u0440\u043A",
+    subtitle: "Один фреймворк",
     imageUrl: "https://angular.io/resources/images/logos/angular2/angular.svg"
-  })
-), document.getElementById('root'));
+}, {
+    id: 3,
+    title: "Ember",
+    subtitle: "Фреймворк для создания амбициозных веб-приложений",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/6/69/Ember.js_Logo_and_Mascot.png"
+}, {
+    id: 4,
+    title: "Vue",
+    subtitle: "Прогрессивный фреймворк",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/1000px-Vue.js_Logo.svg.png"
+}];
+ReactDOM.render(React.createElement(App, { heroes: data }), document.getElementById('root'));
--- app.jsx
+++ app.jsx
@@ -1,34 +1,65 @@
-var Hero = React.createClass({
-  getInitialState: function() {
-    return {
-      count: 0
-    }
-  },
-  HandleClick: function() {
-    this.setState({ count: this.state.count + 1})
-  },
-  render: function() {
+var ImageCounter = function(props) {
     return (
-        <div className="container">
-					<div className="count">{this.state.count}</div>
-          <img src={this.props.imageUrl} onClick={this.HandleClick}/>
-          <h1>{this.props.title}</h1>
-          <p className="subtitle">{this.props.subtitle}</p>
+        <div className="image-counter">
+            <div className="count">{props.count}</div>
+            <img src={props.imageUrl} onClick={props.onCount}/>
         </div>
     );
-  }
+}
+var Hero = React.createClass({
+    getInitialState: function() {
+        return {count: 0}
+    },
+    HandleClick: function() {
+        this.setState({
+            count: this.state.count + 1
+        })
+    },
+    render: function() {
+        return (
+            <div className="container">
+                <ImageCounter imageUrl={this.props.imageUrl} count={this.state.count} onCount={this.HandleClick}/>
+                <h1>{this.props.title}</h1>
+                <p className="subtitle">{this.props.subtitle}</p>
+            </div>
+        );
+    }
 });
-ReactDOM.render(
-  <div>
-    <Hero
-      title="React"
-      subtitle="Библиотека для создания пользовательских интерфейсов"
-      imageUrl="https://facebook.github.io/react/img/logo.svg"
-			/>
-    <Hero
-      title="Angular"
-    	subtitle="Один фреймворк"
-      imageUrl="https://angular.io/resources/images/logos/angular2/angular.svg"
-			/>
-  </div>,
-  document.getElementById('root'))
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.heroes.map((hero) =>
+                    <Hero key={hero.id} title={hero.title} subtitle={hero.subtitle} imageUrl={hero.imageUrl}/>
+                )}
+            </div>
+        );
+    }
+});
+var data = [
+    {
+        id: 1,
+        title: "React",
+        subtitle: "Библиотека для создания пользовательских интерфейсов",
+        imageUrl: "https://facebook.github.io/react/img/logo.svg"
+    },
+    {
+        id: 2,
+        title: "Angular",
+        subtitle: "Один фреймворк",
+        imageUrl: "https://angular.io/resources/images/logos/angular2/angular.svg"
+    },
+    {
+        id: 3,
+        title: "Ember",
+        subtitle: "Фреймворк для создания амбициозных веб-приложений",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/en/6/69/Ember.js_Logo_and_Mascot.png"
+    },
+    {
+        id: 4,
+        title: "Vue",
+        subtitle: "Прогрессивный фреймворк",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/1000px-Vue.js_Logo.svg.png"
+    }
+]
+ReactDOM.render(<App heroes={data}/>, document.getElementById('root'))
